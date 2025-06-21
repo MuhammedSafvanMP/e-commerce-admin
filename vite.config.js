920b5f4595b0a44194ca1b879c6835c5b2e2175f
Muhammed Safvan MP <134296048+MuhammedSafvanMP@users.noreply.github.com>
2025-06-21 13:01:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     preview: {
     host: '0.0.0.0',
     port: 7168,
-    allowedHosts: [''], 
+    allowedHosts: ['e-commerce-dashbord.onrender.com'], 
   },
   resolve: {
     alias: {
