069a9b457aeacf4cffd794b9b01a23c6e3935f47
Muhammed Safvan MP <134296048+MuhammedSafvanMP@users.noreply.github.com>
2025-06-21 13:10:40 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,12 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-   server: {
+  server: {
     host: '0.0.0.0',
     port: 7168,
-  },
-    preview: {
-    host: '0.0.0.0',
-    port: 7168,
-    allowedHosts: ['e-commerce-dashbord.onrender.com'], 
+    allowedHosts: ['e-commerce-dashbord.onrender.com'], // ✅ Correct place
   },
   resolve: {
     alias: {
